cb695dc2964a899f085eb826a6fca59ce64a49d0
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-09-02 14:15:06 -0300

    iniciando Spirng

---
+++ src/main/java/me/dio/SantaderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderDevWeek2023Application.class, args);
+	}
+
+}
